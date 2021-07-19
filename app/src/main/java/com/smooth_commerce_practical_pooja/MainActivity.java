5bafd33ba65e1530da4c3ac4ffd7e8c9990ac116
pooja <poojasonrat95@gmail.com>
2021-07-19 15:02:39 -0400

    second commit

--- app/src/main/java/com/smooth_commerce_practical_pooja/MainActivity.java
+++ app/src/main/java/com/smooth_commerce_practical_pooja/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView rvColors;
     private CustomAdapter customAdapter;
     APIInterface apiInterface;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,14 +42,21 @@ public class MainActivity extends AppCompatActivity {
         btnSearch = (Button) findViewById(R.id.btnSearch);
         rvColors = (RecyclerView) findViewById(R.id.rvColors);
         callcolor("KEYWORDS");
-        rvColors.setLayoutManager(new GridLayoutManager(this,2));
+        rvColors.setLayoutManager(new GridLayoutManager(this, 2));
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(etSearch.getText().toString()!=null&&etSearch.getText().toString().length()>0){
-                    callcolor(etSearch.getText().toString());
-                }else {
-                    Toast.makeText(MainActivity.this, "Please Enter Title First!!!", Toast.LENGTH_SHORT).show();
+                if (etSearch.getText().toString() != null && etSearch.getText().toString().length() > 0) {
+                    if (etSearch.getText().toString().length() < 3 && etSearch.getText().toString().length() > 10) {
+                        Toast.makeText(MainActivity.this, "Please Enter Title in range of 3 to 10 characters!!!", Toast.LENGTH_LONG).show();
+
+                    } else {
+                        callcolor(etSearch.getText().toString());
+                    }
+
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Please Enter Title First!!!", Toast.LENGTH_LONG).show();
                 }
 
             }
@@ -57,15 +65,15 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void callcolor(String keyword){
-        Call<ArrayList<Colors>> call2 = apiInterface.getColors(keyword,"json","20");
+    public void callcolor(String keyword) {
+        Call<ArrayList<Colors>> call2 = apiInterface.getColors(keyword, "json", "20");
         call2.enqueue(new Callback<ArrayList<Colors>>() {
             @Override
             public void onResponse(Call<ArrayList<Colors>> call, Response<ArrayList<Colors>> response) {
 
-                Log.d("responsefrom","responsefrom"+response.body().toString());
-                ArrayList<Colors> colors=response.body();
-                customAdapter=new CustomAdapter(MainActivity.this,colors);
+                Log.d("responsefrom", "responsefrom" + response.body().toString());
+                ArrayList<Colors> colors = response.body();
+                customAdapter = new CustomAdapter(MainActivity.this, colors);
                 rvColors.setAdapter(customAdapter);
 
 
